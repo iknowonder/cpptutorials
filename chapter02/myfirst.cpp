f958893e0f3e37cc1ca5c982331f0280d1027914
iknowonder <iknowonder@gmail.com>
2018-01-25 22:45:41 +0900

    add myfirst

---
+++ chapter02/myfirst.cpp
@@ -0,0 +1,11 @@
+//myfirst.cpp -- output the message
+
+#include <iostream>					//Preprocessing indicator
+int main()						//Function head
+{							//Strat of function body
+	using namespace std;				//Difinition vizualization
+	cout << "Wellcome to the Cpp world";		//Output the message
+	cout << endl;					//Start a new row
+	cout << "You will not regret it!" << endl;	//Output another message
+	return 0;					//Exit main()
+}							//End of funcion body
